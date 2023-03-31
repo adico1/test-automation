6e23c5b2e878ae0d3c6bc27bdddba1cb1c3e8f74
adico <adi.c@sapiens.com>
2023-03-31 20:06:31 +0300

    🎉 Initial commit

--- create-github-repo.sh
+++ create-github-repo.sh
@@ -1,62 +1,67 @@
 #!/bin/bash
 
-# Define some emojis
-SUCCESS_EMOJI="🎉"
-ERROR_EMOJI="❌"
-
-# Define the minimum required version of the GitHub CLI
-MIN_GH_CLI_VERSION="2.25.1"
-
-# Check that the required dependencies are installed
-command -v jq >/dev/null 2>&1 || { echo "${ERROR_EMOJI} jq is required but not installed. Please install it before running this script." >&2; exit 1; }
-command -v gh >/dev/null 2>&1 || { echo "${ERROR_EMOJI} The GitHub CLI is required but not installed. Please install it before running this script: https://cli.github.com" >&2; exit 1; }
-
-# Check that the user is logged in to GitHub
-if ! gh auth status >/dev/null 2>&1; then
-  echo "${ERROR_EMOJI} You are not logged in to GitHub. Please log in using 'gh auth login'." >&2
-  exit 1
+# Check for required dependencies
+if ! command -v gh &> /dev/null; then
+    echo "⚠️ The GitHub CLI (gh) is not installed. Please install it before running this script: https://cli.github.com/manual/installation"
+    exit 1
 fi
 
-# Get the GitHub username from the system
-GITHUB_USERNAME="$(gh config get -h github.com user)"
-if [ -z "$GITHUB_USERNAME" ]; then
-  read -p "Please enter your GitHub username: " GITHUB_USERNAME
+if ! command -v jq &> /dev/null; then
+    echo "⚠️ The jq utility is not installed. Please install it before running this script."
+    exit 1
 fi
 
-# Check the version of the GitHub CLI
-GH_CLI_VERSION="$(gh --version | head -1 | cut -d ' ' -f 3)"
-if [ "$GH_CLI_VERSION" != "$MIN_GH_CLI_VERSION" ]; then
-  echo "${ERROR_EMOJI} The minimum required version of the GitHub CLI is $MIN_GH_CLI_VERSION, but you are running version $GH_CLI_VERSION. Please upgrade before running this script: https://cli.github.com" >&2
-  exit 1
-fi
-
-# Prompt the user for the repository name and description
-if [ $# -eq 1 ]; then
-  REPO_DIR="$1"
-  REPO_NAME="$(basename "$REPO_DIR")"
-  cd "$REPO_DIR"
+# Check if a project path was provided, otherwise use the current directory
+if [ -z "$1" ]; then
+    PROJECT_DIR="."
 else
-  read -p "Please enter the name of the new repository: " REPO_NAME
-  read -p "Please enter a description for the new repository (leave blank for no description): " REPO_DESCRIPTION
-  REPO_DIR="."
+    PROJECT_DIR="$1"
 fi
 
-# Check if the repository name is available
-if gh repo view "$GITHUB_USERNAME/$REPO_NAME" >/dev/null 2>&1; then
-  echo "${ERROR_EMOJI} The repository name $REPO_NAME is not available on GitHub. Please choose a different name." >&2
-  exit 1
+# Check if package.json exists in the project directory
+if [ ! -f "$PROJECT_DIR/package.json" ]; then
+    echo "❌ package.json not found in $PROJECT_DIR. Please provide a repository name and description."
+    read -p "Enter repository name: " REPO_NAME
+    read -p "Enter repository description: " REPO_DESC
+else
+    # Extract repository name and description from package.json
+    REPO_NAME=$(jq -r '.name' "$PROJECT_DIR/package.json")
+    REPO_DESC=$(jq -r '.description' "$PROJECT_DIR/package.json")
+
+    # If package.json does not have a name field, prompt the user for a repository name
+    if [ "$REPO_NAME" = "null" ]; then
+        echo "❌ package.json in $PROJECT_DIR does not have a name field. Please provide a repository name."
+        read -p "Enter repository name: " REPO_NAME
+    fi
+
+    # If package.json does not have a description field, prompt the user for a repository description
+    if [ "$REPO_DESC" = "null" ]; then
+        echo "⚠️ package.json in $PROJECT_DIR does not have a description field. Please provide a repository description."
+        read -p "Enter repository description: " REPO_DESC
+    fi
 fi
 
-# Initialize the Git repository
-git init "$REPO_DIR"
-git add .
-git commit -m "${SUCCESS_EMOJI} Initial commit"
+# Check if repository name is available on GitHub
+if gh repo view "$REPO_NAME" > /dev/null 2>&1; then
+    echo "❌ Repository name $REPO_NAME is already taken on GitHub. Please choose a different name."
+    exit 1
+fi
 
-# Create the new repository on GitHub
-gh repo create "$GITHUB_USERNAME/$REPO_NAME" --description="$REPO_DESCRIPTION" --public
+# Create repository on GitHub
+echo "🚀 Creating repository $REPO_NAME on GitHub..."
+gh repo create "$REPO_NAME" --public --description "$REPO_DESC" > /dev/null
 
-# Add the GitHub repository as a remote and push the local Git repository to the remote
-git remote add origin "https://github.com/$GITHUB_USERNAME/$REPO_NAME.git"
-git push -u origin main
+# Initialize Git repository and make initial commit
+echo "🌱 Initializing Git repository and making initial commit..."
+cd "$PROJECT_DIR"
+git init > /dev/null
+git add . > /dev/null
+git commit -m "🎉 Initial commit" > /dev/null
 
-echo "${SUCCESS_EMOJI} Successfully created repository $REPO_NAME on GitHub!"
+# Push to remote repository on GitHub
+echo "🚀 Pushing initial commit to remote repository on GitHub..."
+git remote add origin "https://github.com/$GITHUB_USERNAME/$REPO_NAME.git" > /dev/null
+git push -u origin main > /dev/null
+
+# Show success message
+echo "🎉 Successfully created repository $REPO_NAME on GitHub!"
