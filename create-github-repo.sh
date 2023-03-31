3f5a446b4280077e7fbedf2c04c8fc924172f7ec
adico <adi.c@sapiens.com>
2023-03-31 16:57:04 +0300

    🎉 Initial commit

--- create-github-repo.sh
+++ create-github-repo.sh
@@ -1,64 +1,62 @@
 #!/bin/bash
 
-# Define emojis
-ROCKET="🚀"
-SMILEY="😄"
-THUMBS_UP="👍"
+# Define some emojis
+SUCCESS_EMOJI="🎉"
+ERROR_EMOJI="❌"
 
-# Check if user is authenticated with GitHub
-if ! gh auth status > /dev/null; then
-  echo "${ROCKET} You need to log in to your GitHub account."
-  gh auth login
-fi
+# Define the minimum required version of the GitHub CLI
+MIN_GH_CLI_VERSION="2.25.1"
 
-# Check if necessary dependencies are installed
-if ! command -v jq > /dev/null; then
-  echo "${ROCKET} jq is required. Please install it and run this script again."
-  echo "You can install jq with Homebrew: 'brew install jq'"
+# Check that the required dependencies are installed
+command -v jq >/dev/null 2>&1 || { echo "${ERROR_EMOJI} jq is required but not installed. Please install it before running this script." >&2; exit 1; }
+command -v gh >/dev/null 2>&1 || { echo "${ERROR_EMOJI} The GitHub CLI is required but not installed. Please install it before running this script: https://cli.github.com" >&2; exit 1; }
+
+# Check that the user is logged in to GitHub
+if ! gh auth status >/dev/null 2>&1; then
+  echo "${ERROR_EMOJI} You are not logged in to GitHub. Please log in using 'gh auth login'." >&2
   exit 1
 fi
 
-# Get project path (if provided)
-if [ -z "$1" ]; then
-  PROJECT_PATH="."
-else
-  PROJECT_PATH="$1"
+# Get the GitHub username from the system
+GITHUB_USERNAME="$(gh config get -h github.com user)"
+if [ -z "$GITHUB_USERNAME" ]; then
+  read -p "Please enter your GitHub username: " GITHUB_USERNAME
 fi
 
-# Initialize Git repository (if necessary)
-if [ ! -d "$PROJECT_PATH/.git" ]; then
-  git init "$PROJECT_PATH"
-  echo "${SMILEY} Initialized Git repository in $PROJECT_PATH"
-fi
-
-# Read repository name and description from package.json (if available)
-if [ -f "$PROJECT_PATH/package.json" ]; then
-  REPO_NAME=$(jq -r '.name' "$PROJECT_PATH/package.json")
-  REPO_DESC=$(jq -r '.description' "$PROJECT_PATH/package.json")
-else
-  # Prompt user for repository name and description
-  echo "${ROCKET} Repository name and description not found in package.json"
-  read -p "Enter repository name: " REPO_NAME
-  read -p "Enter repository description (optional): " REPO_DESC
-fi
-
-# Check if repository name is available on GitHub
-USERNAME=$(gh config get -h github.com user)
-REPO_EXISTS=$(gh api --silent --paginate "/users/$USERNAME/repos" | jq -r ".[].name" | grep -cx "$REPO_NAME")
-if [ "$REPO_EXISTS" -ne 0 ]; then
-  echo "${ROCKET} The repository name '$REPO_NAME' already exists for user '$USERNAME'. Please choose a different name."
+# Check the version of the GitHub CLI
+GH_CLI_VERSION="$(gh --version | head -1 | cut -d ' ' -f 3)"
+if [ "$GH_CLI_VERSION" != "$MIN_GH_CLI_VERSION" ]; then
+  echo "${ERROR_EMOJI} The minimum required version of the GitHub CLI is $MIN_GH_CLI_VERSION, but you are running version $GH_CLI_VERSION. Please upgrade before running this script: https://cli.github.com" >&2
   exit 1
 fi
 
-# Create repository on GitHub
-gh repo create "$REPO_NAME" --public --description "$REPO_DESC" --confirm
+# Prompt the user for the repository name and description
+if [ $# -eq 1 ]; then
+  REPO_DIR="$1"
+  REPO_NAME="$(basename "$REPO_DIR")"
+  cd "$REPO_DIR"
+else
+  read -p "Please enter the name of the new repository: " REPO_NAME
+  read -p "Please enter a description for the new repository (leave blank for no description): " REPO_DESCRIPTION
+  REPO_DIR="."
+fi
 
-# Add remote repository and push changes
-HTML_URL=$(gh repo view "$REPO_NAME" --json html_url | jq -r ".html_url")
-git remote add origin "$HTML_URL"
+# Check if the repository name is available
+if gh repo view "$GITHUB_USERNAME/$REPO_NAME" >/dev/null 2>&1; then
+  echo "${ERROR_EMOJI} The repository name $REPO_NAME is not available on GitHub. Please choose a different name." >&2
+  exit 1
+fi
+
+# Initialize the Git repository
+git init "$REPO_DIR"
 git add .
-git commit -m "${ROCKET} Initial commit"
-git push -u origin main
+git commit -m "${SUCCESS_EMOJI} Initial commit"
 
-# Display success message
-echo "${THUMBS_UP} Repository created successfully at $HTML_URL"
+# Create the new repository on GitHub
+gh repo create "$GITHUB_USERNAME/$REPO_NAME" --description="$REPO_DESCRIPTION" --public --enable-wiki --confirm
+
+# Add the GitHub repository as a remote and push the local Git repository to the remote
+git remote add origin "https://github.com/$GITHUB_USERNAME/$REPO_NAME.git"
+git push -u origin master
+
+echo "${SUCCESS_EMOJI} Successfully created repository $REPO_NAME on GitHub!"
